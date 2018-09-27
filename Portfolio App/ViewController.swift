83c73a654683b8e144bbcf5b1ed944c8cdb46aef
Jamie Cummings <jamiecummings@MacBook-Pro-2.local>
2018-09-27 09:02:10 -0500

    Added initial projects files

---
+++ Portfolio App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Portfolio App
+//
+//  Created by Jamie Cummings on 9/27/18.
+//  Copyright © 2018 Jamie Cummings. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
