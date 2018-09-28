a10d5a73b4843c2275ca2b369bfa79d99397fc1d
Jamie Cummings <jamiecummings@MacBook-Pro-2.local>
2018-09-28 08:00:17 -0500

    1st round of button logo added

--- Portfolio App/InfoViewController.swift
+++ Portfolio App/InfoViewController.swift
@@ -11,10 +11,15 @@ import UIKit
 class InfoViewController: UIViewController {
 
     
+    @IBOutlet weak var gitButton: UIButton!
+    @IBOutlet weak var linkButton: UIButton!
+    @IBOutlet weak var emailButton: UIButton!
     @IBOutlet weak var imageView: UIImageView!
     
-    
     @IBOutlet weak var bioLabel: UILabel!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 // Do any additional setup after loading the view.
@@ -22,10 +27,14 @@ class InfoViewController: UIViewController {
         imageView.backgroundColor = UIColor.cyan 
         imageView.layer.cornerRadius = 8.0
         imageView.clipsToBounds = true
+        emailButton.tintColor = UIColor.cyan
+        linkButton.tintColor = UIColor.cyan
+        gitButton.tintColor = UIColor.cyan
+    
         bioLabel.text = """
-        Jamie is a software developer who helps clients to bring their application ideas to life and maintain existing applications.
+        Jamie is a software developer who helps her clients bring their application ideas to life and maintain existing applications.
 
-        Before starting RheaAnn Software Development or freelance work, Jamie worked in the Barren County School District. Jamie is now training to develop her skills with Swift, iOS and web development.
+        Before starting RheaAnn Software Development, Jamie enjoyed working in the Barren County School District and spending time with her family. Jamie is now training to develop her skills with Swift, iOS and web development.
         """
         bioLabel.textColor = UIColor.white
     }
@@ -40,5 +49,22 @@ class InfoViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
+    
+    func openURL(urlStr:String!){
+        if let url = NSURL(string:urlStr){
+            UIApplication.shared.open(url as URL)
+        }
+    }
 
+    @IBAction func emailLink(_ sender: Any) {
+        
+    }
+    @IBAction func linkLink(_ sender: Any){
+        openURL(urlStr:  "https://www.linkedin.com/in/jamie-cummings-b1948516a/")
+    }
+    
+    @IBAction func gitLink(_ sender: Any) {
+        openURL(urlStr: "https://github.com/JamieCummings")
+    }
+    
 }
